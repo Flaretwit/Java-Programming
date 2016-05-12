63b3726e04ac41430137ea02708182a96c1f7309
Ke Shen <296697@beaverton.k12.or.us>
2016-05-12 11:02:21 -0700

    Worked on frame

--- Snake/src/Gameboard.java
+++ Snake/src/Gameboard.java
@@ -1,4 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 public class Gameboard {
-
+	//contains all outside of game functions, help, 
+	Container east = new Container();
+	
+	JFrame frame = new JFrame("Snake (Slither.io)");
+	Run run = new Run();
+	public Gameboard() {
+		frame.setSize(1000,800);
+		frame.setLayout(new BorderLayout());
+		frame.add(run, BorderLayout.CENTER);
+		
+		east.setLayout(new BorderLayout());
+		east.setSize(800,200);
+		
+		frame.add(east, BorderLayout.EAST);
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.repaint();
+		frame.setResizable(false);
+		
+		
+		
+	
+	}
+	
+	public static void main(String[] args) {
+		new Gameboard();
+	}
+	
 }
--- Snake/src/Run.java
+++ Snake/src/Run.java
@@ -1,4 +1,27 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import java.awt.Color;
 
-public class Run {
-
+public class Run extends JPanel {
+	//this will contain the gameboard and track what each grid is
+	final int BLANK = 0;
+	final int FOOD = 1;
+	final int OCCUPIED = 2;
+	
+	
+	int[][] field = new int[40][40]; //20 pixels per grid for now
+	
+	
+	public Run() {
+		super();
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.RED);
+		g.fillRect(800, 0, getWidth(), getHeight());
+		
+	}
 }
