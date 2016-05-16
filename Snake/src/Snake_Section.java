b1be2c1480ec33bbd6a170e3c94a6a10ae4d8b55
Ke Shen <296697@beaverton.k12.or.us>
2016-05-16 09:13:26 -0700

    Added methods for death and movement

--- Snake/src/Snake_Section.java
+++ Snake/src/Snake_Section.java
@@ -1,7 +1,7 @@
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
-public class Snake_Section {
+public class Snake_Section  {
 	//0 is facing up, 1 is facing east, 2 is facing south, 3 is facing west
 	//(randomly chosen upon snake spawn)
 	int direction;
@@ -9,6 +9,23 @@ public class Snake_Section {
 	int length = snakeloc.size();
 	Point head = snakeloc.get(0);
 	
+	
+	public int getDir() {
+		return direction;
+	}
+	public void setDir(int dir) {
+		direction = dir;
+	}
+	
+	public void setPoint(int x, int y) {
+		Point tail = new Point();
+		tail.setX(x);
+		tail.setY(y);
+		snakeloc.add(tail);
+	}
+	public Point getPoint(int i) {
+		return snakeloc.get(i);
+	}
 	
 }
 
